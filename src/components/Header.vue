021d72728a085ee1de9c8395f0ca00a974b1ec65
Alisher1504 <ruzmetov150499@gmail.com>
2023-12-03 18:57:40 +0500

    add header and footer in home page

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+
+<script>
+
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container ps-5">
+            <a class="navbar-brand" href="#">
+                Logo
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TV Shows</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">People</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">More</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style>
+.navbar {
+
+    background-color: #032541;
+    height: 64px;
+
+}
+
+.navbar .container .collapse .navbar-nav .nav-item .nav-link {
+
+    color: white;
+    font-weight: 600;
+    margin-left: 10px;
+
+}
+
+.navbar .container .navbar-brand {
+
+    color: white;
+
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,37 @@
+
+<script>
+
+</script>
+
+<template>
+    
+    <section>
+
+        <div class="container">
+
+            <div class="col-12 image ">
+                
+                
+
+            </div>
+
+        </div>
+
+    </section>
+
+</template>
+
+<style>
+
+    .container .image {
+
+        background-image: url('../../public/images/movie/straji.jpg');
+        min-height: 360px;
+        height: calc(100vh/2.5);
+        max-height: 360px;
+        background-size: cover;
+        object-fit: contain;
+
+    }
+
+</style>
